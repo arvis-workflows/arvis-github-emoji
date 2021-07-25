bc3ed6fcde587d167402475be181684eec58eee0
jopemachine <jopemachine@naver.com>
2021-07-25 16:05:56 +0900

    Upd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ arvish.fetch('https://api.github.com/emojis', { maxAge: 24 * 60 * 60 }).then((em
     const items = Object.keys(emojis).map((emojiName) => {
         return {
             title: emojiName,
-            subtitle: `Press enter to copy emoji name`,
+            subtitle: `Press enter to copy ":${emojiName}:"`,
             icon: emojis[emojiName],
             arg: `:${emojiName}:`,
             quicklookurl: emojis[emojiName],
